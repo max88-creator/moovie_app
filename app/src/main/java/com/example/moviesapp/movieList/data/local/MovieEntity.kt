c7c5f4a7e265327a593e122e5f4e28c3ce565fc4
Maksim <maxgrank88@gmail.com>
2026-05-09 15:49:08 +0300

    created RatingBar

--- app/src/main/java/com/example/moviesapp/movieList/data/local/MovieEntity.kt
+++ app/src/main/java/com/example/moviesapp/movieList/data/local/MovieEntity.kt
@@ -7,19 +7,19 @@ import com.example.moviesapp.movieList.data.remote.respond.MovieDto
 @Entity(tableName = "movie_ent")
 data class MovieEntity(
     @PrimaryKey
-    val id: Int?,
+    val id: Int,
     val category: String,
-    val adult: Boolean?,
-    val backdrop_path: String?,
-    val genre_ids: List<Int>?,
-    val original_language: String?,
-    val original_title: String?,
-    val overview: String?,
-    val popularity: Double?,
-    val poster_path: String?,
-    val release_date: String?,
-    val title: String?,
-    val video: Boolean?,
-    val vote_average: Double?,
-    val vote_count: Int?
+    val adult: Boolean,
+    val backdrop_path: String,
+    val genre_ids: String,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
 )
